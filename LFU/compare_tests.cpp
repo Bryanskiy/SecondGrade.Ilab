af6452f0ac84d01b506fb88f7864050b4e8abfbc
Bryanskiy <ivakin.ki@phystech.edu>
2020-09-10 22:04:41 +0300

    small changes

--- LFU/compare_tests.cpp
+++ LFU/compare_tests.cpp
@@ -64,59 +64,31 @@ stat_t cache_statistic(C& cache, std::size_t request_count, D& distribution) {
     return stat;
 }
 
-/*
-template<template<typename> class C, typename D>
-void write_statistic(char const * const file_name, D& distribution) {
-    std::size_t request_count = 1000000;
-
+template<typename C, typename D>
+void write_statistic(char const * const file_name, std::size_t request_count, D& distribution)  {
     std::ofstream out;
     out.open(file_name);
 
-    for(std::size_t i = 10; i <= 20; ++i) {
-        C<page_t> cache_(i);
+    for(std::size_t i = 10; i <= 200; ++i) {
+        C cache(i);
         out << i << ' ';
-        stat_t stat = cache_stat(cache_, request_count, distribution);
+        stat_t stat = cache_statistic(cache, request_count, distribution);
         out << stat.hits << ' ' << stat.time.count() << std::endl;
     }
 
     out.close();
 }
 
-std::uniform_int_distribution<> dist(0, 1000);
-write_statistic<LFU_t>("LFU_uniform_int_distribution.txt", dist); ????????????????????????????
-*/
-
 void collect_statistic() {
-    std::size_t request_count = 1000000;
-
+    std::size_t request_count = 100000;
     {
-        std::uniform_int_distribution<> distribution(0, 1000);
-        std::ofstream out;
-        out.open("statistic/LFU_uniform_int_distribution.txt");
-
-        for(std::size_t i = 10; i <= 20; ++i) {
-            LFU_t<page_t> cache(i);
-            out << i << ' ';
-            stat_t stat = cache_statistic(cache, request_count, distribution);
-            out << stat.hits << ' ' << stat.time.count() << std::endl;
-        }
-
-        out.close();
+        std::uniform_int_distribution<> dist(0, 1000);
+        write_statistic<LFU_t<page_t>>("statistic/LFU_uniform_int_distribution.txt", request_count, dist);
     }
 
     {
-        std::uniform_int_distribution<> distribution(0, 1000);
-        std::ofstream out;
-        out.open("statistic/LRU_uniform_int_distribution.txt");
-
-        for(std::size_t i = 10; i <= 20; ++i) {
-            LRU_t<page_t> cache(i);
-            out << i << ' ';
-            stat_t stat = cache_statistic(cache, request_count, distribution);
-            out << stat.hits << ' ' << stat.time.count() << std::endl;
-        }
-
-        out.close();
+        std::uniform_int_distribution<> dist(0, 1000);
+        write_statistic<LRU_t<page_t>>("statistic/LRU_uniform_int_distribution.txt", request_count, dist);
     }
 }
 
