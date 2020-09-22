5eea4b66bd6f4c55a536841bc70fd276d5405c3f
Bryanskiy <ivakin.ki@phystech.edu>
2020-09-22 22:44:04 +0300

    change main

--- triangles/main.cpp
+++ triangles/main.cpp
@@ -11,7 +11,7 @@ int main() {
 
     for(int i = 0; i < n - 1; ++i) {
         for(int j = i + 1; j < n; ++j) {
-            if(intersection_of_2triangles_3D(triangles[i], triangles[j])) {
+            if(triangles[i].intersect(triangles[j])) {
                 std::cout << i << ' ' << j << ' ';
             }
         }
