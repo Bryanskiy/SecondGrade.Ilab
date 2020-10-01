7a623e523f9697e45fe1182c2a590d12c5655212
Bryanskiy <ivakin.ki@phystech.edu>
2020-10-01 19:26:51 +0300

    add vector

--- geometry/geometry.h
+++ geometry/geometry.h
@@ -28,9 +28,10 @@ namespace ivkg {
     class point_t {
 
     public:
-        point_t<dim_>();
+        point_t();
         point_t(const point_t<dim_> &) = default;
         point_t(std::initializer_list <coordinate_t> coordinates);
+        ~point_t() = default;
 
         bool valid() const;
         bool operator==(const point_t<dim_>& rhs) const;
@@ -49,6 +50,61 @@ namespace ivkg {
 
     template<std::size_t DIM_>
     std::ostream &operator<<(std::ostream &out, const point_t<DIM_> &point);
+
+
+    /* ------------------------------------------------
+                      VECTOR_CLASS
+    -------------------------------------------------*/
+    template<std::size_t dim_>
+    class vector_t {
+        public:
+            vector_t();
+            vector_t(const vector_t<dim_> &) = default;
+            vector_t(std::initializer_list<coordinate_t> coordinates);
+            vector_t(const point_t<dim_>& lhs, const point_t<dim_>& rhs);
+            ~vector_t() = default;
+
+            coordinate_t &operator[](std::size_t idx);
+            const coordinate_t &operator[](std::size_t idx) const;
+
+            vector_t<dim_>& operator+=(const vector_t<dim_>& rhs);
+            vector_t<dim_>& operator-=(const vector_t<dim_>& rhs);
+            vector_t<dim_>& operator*=(long double lambda);
+            const vector_t<dim_> operator-();
+
+            bool valid() const;
+            long double len() const;
+            bool zero() const;
+        private:
+            std::array <coordinate_t, dim_> coordinates_;
+    };
+
+    template<std::size_t DIM_>
+    long double dot(const vector_t<DIM_>& lhs, const vector_t<DIM_>& rhs);
+
+    vector_t<3> cross(const vector_t<3>& lhs, const vector_t<3>& rhs);
+
+    template<std::size_t DIM_>
+    bool operator==(const vector_t<DIM_>& lhs, const vector_t<DIM_>& rhs);
+    template<std::size_t DIM_>
+    const vector_t<DIM_> operator+(const vector_t<DIM_>& lhs, const vector_t<DIM_>& rhs);
+    template<std::size_t DIM_>
+    const vector_t<DIM_> operator-(const vector_t<DIM_>& lhs, const vector_t<DIM_>& rhs);
+    template<std::size_t DIM_>
+    const vector_t<DIM_> operator*(long double lambda, const vector_t<DIM_>& rhs);
+    template<std::size_t DIM_>
+    const vector_t<DIM_> operator*(const vector_t<DIM_>& lhs, long double lambda);
+
+    template<std::size_t DIM_>
+    bool parallel(const vector_t<DIM_>& lhs, const vector_t<DIM_>& rhs);
+
+    template<std::size_t DIM_>
+    std::istream& operator>>(std::istream& in, vector_t<DIM_>& vector);
+
+    /*
+    template<std::size_t DIM_>
+    std::ostream& operator<<(std::ostream& out, const vector_t<DIM_>& vector);
+    */
 }
 
 
@@ -131,3 +187,219 @@ template<std::size_t dim_>
 const ivkg::coordinate_t& ivkg::point_t<dim_>::operator[](std::size_t idx) const {
     return coordinates_[idx];
 }
+
+template<std::size_t dim_>
+long double ivkg::point_t<dim_>::distance() const {
+    return ivkg::vector_t<dim_>(coordinates_).len();
+}
+
+
+/* ------------------------------------------------
+                REALIZATION_VECTOR_CLASS
+-------------------------------------------------*/
+template<std::size_t dim_>
+ivkg::vector_t<dim_>::vector_t() {
+    for(coordinate_t& elem : coordinates_) {
+        elem = std::numeric_limits<double>::quiet_NaN();
+    }
+}
+
+
+//todo: avoid duplicate code (the same constructor in point class)
+template<std::size_t dim_>
+ivkg::vector_t<dim_>::vector_t(std::initializer_list<coordinate_t> coordinates) {
+    if(coordinates.size() != dim_) {
+        return;
+    }
+
+    typename std::array<coordinate_t, dim_>::iterator obj_iter  = coordinates_.begin();
+    typename std::initializer_list<coordinate_t>::iterator init_iter = coordinates.begin();
+    while (obj_iter != coordinates_.end()) {
+        *obj_iter = *init_iter;
+        obj_iter++;
+        init_iter++;
+    }
+}
+
+//todo: construct vector from points with different dimensions
+template<std::size_t dim_>
+ivkg::vector_t<dim_>::vector_t(const point_t<dim_>& lhs, const point_t<dim_>& rhs) {
+    for(std::size_t i = 0; i < dim_; ++i) {
+        coordinates_[i] = rhs[i] - lhs[i];
+    }
+}
+
+template<std::size_t dim_>
+ivkg::vector_t<dim_>& ivkg::vector_t<dim_>::operator+=(const vector_t<dim_>& rhs) {
+    for(std::size_t i = 0; i < dim_; ++i) {
+        this->coordinates_[i] += rhs[i];
+    }
+
+    return *this;
+}
+
+//todo: avoid duplicate code (the same in operator +=)
+template<std::size_t dim_>
+ivkg::vector_t<dim_>& ivkg::vector_t<dim_>::operator-=(const vector_t<dim_>& rhs) {
+    for(std::size_t i = 0; i < dim_; ++i) {
+        this->coordinates_[i] -= rhs[i];
+    }
+
+    return *this;
+}
+
+//todo: overflow protection
+template<std::size_t dim_>
+ivkg::vector_t<dim_>& ivkg::vector_t<dim_>::operator*=(long double lambda) {
+    for(std::size_t i = 0; i < dim_; ++i) {
+        this->coordinates_[i] *= lambda;
+    }
+
+    return *this;
+}
+
+//todo: avoid duplicate code(the same in point class)
+template<std::size_t dim_>
+bool ivkg::vector_t<dim_>::valid() const {
+    bool flag = true;
+    for(const coordinate_t& elem : coordinates_) {
+        flag = flag && ivkg::valid(elem);
+    }
+
+    return flag;
+}
+
+template<std::size_t dim_>
+long double ivkg::vector_t<dim_>::len() const {
+    return sqrt(dot(*this, *this));
+}
+
+template<std::size_t dim_>
+bool ivkg::vector_t<dim_>::zero() const {
+    if(!this->valid()) {
+        return false;
+    }
+
+
+    for(std::size_t i = 0; i < dim_; ++i) {
+        if(!equal(this->coordinates_[i], 0.0)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+template<std::size_t dim_>
+const ivkg::vector_t<dim_> ivkg::vector_t<dim_>::operator-() {
+    ivkg::vector_t<dim_> ret;
+    for(std::size_t i = 0; i < dim_; ++i) {
+        ret[i] = -coordinates_[i];
+    }
+
+    return ret;
+}
+
+template<std::size_t dim_>
+ivkg::coordinate_t &ivkg::vector_t<dim_>::operator[](std::size_t idx) {
+    return coordinates_[idx];
+}
+
+template<std::size_t dim_>
+const ivkg::coordinate_t &ivkg::vector_t<dim_>::operator[](std::size_t idx) const {
+    return coordinates_[idx];
+}
+
+//todo: for vectors with different dimensions
+template<std::size_t DIM_>
+long double ivkg::dot(const ivkg::vector_t<DIM_>& lhs, const ivkg::vector_t<DIM_>& rhs) {
+    if(!lhs.valid() || !rhs.valid()) {
+        return std::numeric_limits<double>::quiet_NaN();
+    }
+
+    long double ans = 0;
+    for(std::size_t i = 0; i < DIM_; ++i) {
+        ans += lhs[i] * rhs[i];
+    }
+
+    return ans;
+}
+
+//todo: for all dimensions
+ivkg::vector_t<3> ivkg::cross(const ivkg::vector_t<3>& lhs, const ivkg::vector_t<3>& rhs) {
+    ivkg::coordinate_t x = lhs[1] * rhs[2] - lhs[2] * rhs[1];
+    ivkg::coordinate_t y = lhs[2] * rhs[0] - lhs[0] * rhs[2];
+    ivkg::coordinate_t z = lhs[0] * rhs[1] - lhs[1] * rhs[0];
+    return ivkg::vector_t<3>({x, y, z});
+}
+
+template<std::size_t DIM_>
+bool ivkg::operator==(const ivkg::vector_t<DIM_>& lhs, const ivkg::vector_t<DIM_>& rhs) {
+    if(!lhs.valid() || !rhs.valid()) {
+        return false;
+    }
+
+    for(std::size_t i = 0; i < DIM_; ++i) {
+        if(!equal(lhs[i], rhs[i])) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+template<std::size_t DIM_>
+const ivkg::vector_t<DIM_> ivkg::operator+(const ivkg::vector_t<DIM_>& lhs, const ivkg::vector_t<DIM_>& rhs) {
+    ivkg::vector_t<DIM_> tmp = lhs;
+    return tmp += rhs;
+}
+
+template<std::size_t DIM_>
+const ivkg::vector_t<DIM_> ivkg::operator-(const ivkg::vector_t<DIM_>& lhs, const ivkg::vector_t<DIM_>& rhs) {
+    ivkg::vector_t<DIM_> tmp = lhs;
+    return tmp -= rhs;
+}
+
+template<std::size_t DIM_>
+const ivkg::vector_t<DIM_> ivkg::operator*(long double lambda, const ivkg::vector_t<DIM_>& rhs) {
+    ivkg::vector_t<DIM_> tmp = rhs;
+    return tmp *= lambda;
+}
+
+template<std::size_t DIM_>
+const ivkg::vector_t<DIM_> ivkg::operator*(const ivkg::vector_t<DIM_>& lhs, long double lambda) {
+    return lambda * lhs;
+}
+
+template<std::size_t DIM_>
+bool ivkg::parallel(const ivkg::vector_t<DIM_>& lhs, const ivkg::vector_t<DIM_>& rhs) {
+    if(!lhs.valid() || !rhs.valid()) {
+        return false;
+    }
+
+    return equal(dot(lhs, rhs), lhs.len() * rhs.len());
+}
+
+template<std::size_t DIM_>
+std::istream& operator>>(std::istream& in, ivkg::vector_t<DIM_>& vector) {
+    for(std::size_t i = 0; i < DIM_; ++i) {
+        in >> vector[i];
+    }
+
+    return in;
+}
+
+/*
+template<std::size_t DIM_>
+std::ostream& operator<<(std::ostream& out, const ivkg::vector_t<DIM_>& vector) {
+    out << '(';
+    out << vector[0];
+    for(std::size_t i = 1; i < DIM_; ++i) {
+        out << ", ";
+        out << vector[i];
+    }
+
+    out << ')';
+    return out;
+}
+*/
--- geometry/unit_tests.cpp
+++ geometry/unit_tests.cpp
@@ -4,6 +4,9 @@
 
 #include "geometry.h"
 
+/* ------------------------------------------------
+                START POINT_TESTS
+ -------------------------------------------------*/
 TEST(GeometryPointTest, IOtest) {
     {
         std::istringstream in("1.0 2.0 3.0");
@@ -15,7 +18,7 @@ TEST(GeometryPointTest, IOtest) {
 
     {
         std::ostringstream out;
-        ivkg::point_t<3> p({1.1, 2.2, 3.3}); out << p;
+        ivkg::point_t<3> p{1.1, 2.2, 3.3}; out << p;
         std::string expected = "(1.1, 2.2, 3.3)";
         std::string actual = out.str();
         ASSERT_EQ(expected, actual);
@@ -36,14 +39,14 @@ TEST(GeometryPointTest, ValidTest) {
 
 TEST(GeometryPointTest, CompareTest) {
     {
-        ivkg::point_t<3> x1({1.0, 5.0, 3.0});
-        ivkg::point_t<3> x2({1.0, 5.0, 3.0});
+        ivkg::point_t<3> x1{1.0, 5.0, 3.0};
+        ivkg::point_t<3> x2{1.0, 5.0, 3.0};
         ASSERT_TRUE(x1 == x2);
     }
 
     {
-        ivkg::point_t<3> x1({1.0, 2.0, 3.0});
-        ivkg::point_t<3> x2({0.0, 0.0, 0.0});
+        ivkg::point_t<3> x1{1.0, 2.0, 3.0};
+        ivkg::point_t<3> x2{0.0, 0.0, 0.0};
         ASSERT_FALSE(x1 == x2);
     }
 
@@ -51,6 +54,195 @@ TEST(GeometryPointTest, CompareTest) {
         ivkg::point_t<3> x1({1.0, 2.0, 3.0});
         ivkg::point_t<3> x2;
         ASSERT_FALSE(x1 == x2) << "x2 - invalid point";
+    }
+}
+
+/* ------------------------------------------------
+                START VECTOR_TESTS
+ -------------------------------------------------*
+*/
+TEST(GeometryVectorTest, Arithmetic) {
+    {
+        ivkg::vector_t<3> a{1.0, 2.0, 3.0};
+        ivkg::vector_t<3> b{0.0, 4.0, 3.0};
+        ivkg::vector_t<3> expected{1.0, 6.0, 6.0};
+        ASSERT_EQ(a + b, expected);
+        ASSERT_EQ(a += b, expected);
+    }
+
+    {
+        ivkg::vector_t<3> a{1.0, 2.0, 3.0};
+        ivkg::vector_t<3> b{0.0, 4.0, 3.0};
+        ivkg::vector_t<3> expected{1.0, -2.0, 0.0};
+        ASSERT_EQ(a - b, expected);
+        ASSERT_EQ(a -= b, expected);
+    }
+
+    {
+        ivkg::vector_t<3> a{1.0, 2.0, 3.0};
+        ivkg::vector_t<3> a_copy{1.0, 2.0, 3.0};
+        ivkg::vector_t<3> expected{-1.0, -2.0, -3.0};
+        ASSERT_EQ(-a, expected);
+        ASSERT_EQ(a, a_copy) << "unary sub modify vector";
+    }
+
+    {
+        ivkg::vector_t<3> a{1.0, 2.0, 3.0};
+        double lambda = 4.0;
+        ivkg::vector_t<3> expected{4.0, 8.0, 12.0};
+        ASSERT_EQ(a * lambda, expected) << "mult by positive lambda";
+        ASSERT_EQ(a *= lambda, expected) << "mult by positive lambda";
+    }
+
+    {
+        ivkg::vector_t<3> a{1.0, 2.0, 0.0};
+        double lambda = 0.0;
+        ivkg::vector_t<3> expected{0.0, 0.0, 0.0};
+        ASSERT_EQ(a * lambda, expected) << "mult by zero lambda";
+        ASSERT_EQ(a *= lambda, expected) << "mult by zero lambda";
+    }
+
+    {
+        ivkg::vector_t<3> a{1.0, 2.0, 0.0};
+        double lambda = -1.0;
+        ivkg::vector_t<3> expected{-1.0, -2.0, 0.0};
+        ASSERT_EQ(a * lambda, expected) << "mult by negative lambda";
+        ASSERT_EQ(a *= lambda, expected) << "mult by negative lambda";
+    }
+}
+
+TEST(GeometryVectorTest, Valid) {
+    {
+        ivkg::vector_t<10> x;
+        ASSERT_FALSE(x.valid()) << "default construct";
+    }
+
+    {
+        ivkg::point_t<3> x1{1.0, 2.0, 3.0};
+        ivkg::point_t<3> x2{1.0, 3.0, 5.0};
+        ivkg::vector_t<3> x{x1, x2};
+        ASSERT_TRUE(x.valid()) << "construct from 2 valid points";
+    }
+
+    {
+        ivkg::point_t<3> x1;
+        ivkg::point_t<3> x2{1.0, 3.0, 5.0};
+        ivkg::vector_t<3> x{x1, x2};
+        ASSERT_FALSE(x.valid()) << "construct from 1 invalid and 1 valid points";
+    }
+
+    {
+        ivkg::point_t<3> x1;
+        ivkg::point_t<3> x2;
+        ivkg::vector_t<3> x(x1, x2);
+        ASSERT_FALSE(x.valid()) << "construct from 2 invalid points";
+    }
+
+    {
+        ivkg::vector_t<3> x{1.0, 2.0, 3.0};
+        ASSERT_TRUE(x.valid()) << "construct doubles vector";
+    }
+
+}
+
+TEST(GeometryVectorTest, Len) {
+    {
+        ivkg::vector_t<3> v{3.0, 4.0, 0.0};
+        ASSERT_DOUBLE_EQ(v.len(), 5.0);
+    }
+
+    {
+        ivkg::vector_t<3> v{0.0, 0.0, 0.0};
+        ASSERT_DOUBLE_EQ(v.len(), 0.0);
+    }
+
+    {
+        ivkg::vector_t<3> v{16.0, 0.0, 0.0};
+        ASSERT_DOUBLE_EQ(v.len(), 16.0);
+    }
+}
+
+TEST(GeometryVectorTest, IsZero) {
+    {
+        ivkg::vector_t<3> v{3.0, 4.0, 0.0};
+        ASSERT_FALSE(v.zero());
+    }
+
+    {
+        ivkg::vector_t<3> v{0.0, 0.0, 0.0};
+        ASSERT_TRUE(v.zero());
+    }
+
+    {
+        ivkg::vector_t<3> v;
+        ASSERT_FALSE(v.zero());
+    }
+}
+
+TEST(GeometryVectorTest, Equal) {
+    {
+        ivkg::vector_t<3> v1{.0, 4.0, 0.0};
+        ivkg::vector_t<3> v2{.0, 4.0, 0.0};
+        ASSERT_TRUE(v1 == v2);
+    }
+
+    {
+        ivkg::vector_t<3> v1{0.0, 4.0, 0.0};
+        ivkg::vector_t<3> v2{0.0, 4.0, 0.0};
+        ASSERT_TRUE(v1 == v2);
+    }
+
+    {
+        ivkg::vector_t<3> v1{0.0, 4.0, 0.0};
+        ivkg::vector_t<3> v2;
+        ASSERT_FALSE(v1 == v2) << "1 invalid vector";
+    }
+
+    {
+        ivkg::vector_t<3> v1{3.0, 4.0, 0.0};
+        ivkg::point_t<3> p1{1.0, 0.0, 0.0};
+        ivkg::point_t<3> p2{4.0, 4.0 , 0.0};
+        ivkg::vector_t<3> v2(p2, p1);
+        ASSERT_FALSE(v1 == v2) << "1 vector construct from points";
+    }
+}
+
+
+TEST(GeometryVectorTest, IsParallel) {
+    {
+        ivkg::vector_t<3> v1{3.0, 4.0, 0.0};
+        ivkg::vector_t<3> v2{6.0, 8.0, 0.0};
+        ASSERT_TRUE(parallel(v1, v2));
+    }
+
+    {
+        ivkg::vector_t<3> v1{3.0, 4.0, 0.0};
+        ivkg::vector_t<3> v2{1.0, 8.0, 0.0};
+        ASSERT_FALSE(parallel(v1, v2));
+    }
+
+    {
+        ivkg::vector_t<3> v1{3.0, 4.0, 0.0};
+        ivkg::vector_t<3> v2;
+        ASSERT_FALSE(parallel(v1, v2)) << "1 vector invalid";
+    }
+}
+
+
+TEST(GeometryVectorTest, Dot) {
+    {
+        ivkg::vector_t<3> a{1.0, 2.0, 3.0};
+        ivkg::vector_t<3> b{0.0, 4.0, 3.0};
+        ASSERT_DOUBLE_EQ(17.0, dot(a, b));
+    }
+}
+
+TEST(GeometryVectorTest, Cross) {
+    {
+        ivkg::vector_t<3> a{1.0, 2.0, 3.0};
+        ivkg::vector_t<3> b{-2.0, 0.0, 4.0};
+        ivkg::vector_t<3> expected{8.0, -10.0, 4.0};
+        ASSERT_EQ(expected, cross(a, b));
     }
 }
 
