32e453d686bcd3063973165392e6d05bcc2bca24
Bryanskiy <ivakin.ki@phystech.edu>
2020-10-15 21:35:14 +0300

    rename

--- geometry/build.bash
+++ geometry/build.bash
@@ -2,4 +2,4 @@
 
 g++ ./main.cpp  -o main.out
 g++ ./testing/stress_tests/algorithm.cpp -o stress_tests.out
-g++ ./testing/unit_tests/main.cpp -lgtest -lpthread -o unit_tests.out
+g++ ./testing/geometry_unit_tests/main.cpp -lgtest -lpthread -o unit_tests.out
