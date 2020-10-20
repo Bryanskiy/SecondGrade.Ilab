cf521f79370f436dcb5aea3683ca76ca717bab74
Bryanskiy <ivakin.ki@phystech.edu>
2020-10-20 12:14:36 +0300

    change main

--- geometry/main.cpp
+++ geometry/main.cpp
@@ -1,3 +1,4 @@
+
 #include "src/include/geometry.h"
 #include "src/include/octtree.h"
 
@@ -5,13 +6,13 @@
 
 int main() {
     std::size_t N; std::cin >> N;
-    std::vector<ivkg::triangle_t<3>> triangles(N);
+    std::vector<lingeo::triangle_t<3>> triangles(N);
     for(std::size_t i = 0; i < N; ++i) {
         std::cin >> triangles[i];
     }
 
     octt::space_t space = octt::create(triangles);
-    octt::octtree_t<ivkg::triangle_t<3>> octtree(space);
+    octt::octtree_t<lingeo::triangle_t<3>> octtree(space);
 
     for(std::size_t i = 0; i < N; ++i) {
         octtree.insert(i, triangles[i]);
