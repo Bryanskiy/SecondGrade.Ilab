fc389c8aef84eca8e8032191681b173aa2bc8476
Bryanskiy <ivakin.ki@phystech.edu>
2021-02-16 12:46:37 +0300

    change input stream

--- matrix_2/main.cpp
+++ matrix_2/main.cpp
@@ -6,11 +6,11 @@
 #include "matrix/matrix.hpp"
 #include "parser/driver.hpp"
 
-int main(int argc, char** argv) {
-    yy::driver_t driver(argv[1]);
+int main() {
+    yy::driver_t driver;
     driver.parse();
     std::vector<circuit::edge_t> edges = driver.get_edges();
     circuit::circuit_t circuit(edges);
     circuit.calculate_currents();   
-    circuit.print_currents();                    
+    circuit.print_currents();               
 }
