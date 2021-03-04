2d128d3a4093586d78e1892914bf2b19f9a19ee5
Bryanskiy <ivakin.ki@physteh.edu>
2021-03-04 21:37:53 +0300

    create graph representation

--- combinatorics_1/kgraph.hpp
+++ combinatorics_1/kgraph.hpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <vector>
 #include <stdexcept>
+#include <unordered_map>
 #include <iomanip>
 	
 
@@ -13,14 +14,15 @@ class kgraph_t final {
 public:
     kgraph_t() : vertex_size_(0u), vertex_capacity_(2u) {graph_.resize(vertex_capacity_);}
 
-    /* retern value - vertex id */
-    std::size_t push_vertex(const VT& vertex_data = VT{});
     /* v1, v2 - vertices id */
     void push_edge(std::size_t v1, std::size_t v2, const ET& edge_data = ET{});
     void dump(std::ostream& out) const;
 
 private:
     void vertex_realloc(std::size_t new_vertex_capacity);
+    /*  v1      - user id
+        retern  - internal idx */
+    std::size_t push_vertex(std::size_t v);
 private:
     /* essense can be vertex or edge */
     struct essense_t {
@@ -38,47 +40,61 @@ private:
     std::vector<VT> vertex_data_;
     /* edge_data_[i] matchs to graph_[i + vertex_capacity_] edge */
     std::vector<ET> edge_data_;
+
+    /* bijection between iser id's and internal idx for vertices */
+    std::unordered_map<std::size_t, std::size_t> user2internal_;
+    std::unordered_map<std::size_t, std::size_t> internal2user_;
 };
 
+
 template<typename VT, typename ET>
-std::size_t kgraph_t<VT, ET>::push_vertex(const VT& vertex_data /* = VT{} */) {
-    if(vertex_size_ == vertex_capacity_) {
-        vertex_realloc(vertex_capacity_ * 2);
+std::size_t kgraph_t<VT, ET>::push_vertex(std::size_t v) {
+    if(user2internal_.find(v) != user2internal_.end()) {
+        return user2internal_[v];
     }
 
-    graph_[vertex_size_] = {0, vertex_size_, vertex_size_};
+    if(vertex_size_ == vertex_capacity_) {
+        vertex_realloc(vertex_capacity_ * 2u);
+    }
+
+    std::size_t ret = user2internal_[v] = vertex_size_;
+    internal2user_[vertex_size_] = v;
+    
+    graph_[ret] = {0, ret, ret};
+
     ++vertex_size_;
-#ifdef DEBUG
-    dump(std::cout);
-#endif    
-    return vertex_size_;
+    return ret;
 }
 
 template<typename VT, typename ET>
 void kgraph_t<VT, ET>::push_edge(std::size_t v1, std::size_t v2, const ET& edge_data /* = ET{} */ ) {
-    if((v1 > vertex_size_) || (v2 > vertex_size_)) {
-        throw std::runtime_error("Vertex didn't exist");
-    }
+    std::size_t internal_v1 = push_vertex(v1);
+    std::size_t internal_v2 = push_vertex(v2);
 
     /* first essesnse */
     {
-        std::size_t last_essense = graph_[v1 - 1].prev;
-        graph_[v1 - 1].prev = graph_.size();
+        std::size_t last_essense = graph_[user2internal_[v1]].prev;
+        graph_[user2internal_[v1]].prev = graph_.size();
         graph_[last_essense].next = graph_.size();
-        graph_.push_back({v1, v1 - 1, last_essense});
+        graph_.push_back({v1, user2internal_[v1], last_essense});
     }
+
+ #ifdef DEBUG
+    dump(std::cout);
+#endif     
 
     /* second essesnse */
     {
-        std::size_t last_essense = graph_[v2 - 1].prev;
-        graph_[v2 - 1].prev = graph_.size();
+        std::size_t last_essense = graph_[user2internal_[v2]].prev;
+        graph_[user2internal_[v2]].prev = graph_.size();
         graph_[last_essense].next = graph_.size();
-        graph_.push_back({v2, v2 - 1, last_essense});
+        graph_.push_back({v2, user2internal_[v2], last_essense});
     }
 
 #ifdef DEBUG
     dump(std::cout);
-#endif   
+#endif 
+      
 }
 
 template<typename VT, typename ET>
@@ -105,29 +121,29 @@ void kgraph_t<VT, ET>::vertex_realloc(std::size_t new_vertex_capacity) {
 template<typename VT, typename ET>
 void kgraph_t<VT, ET>::dump(std::ostream& out) const {
     for(std::size_t i = 0, maxi = graph_.size(); i < maxi; ++i) {
-        if((i >= vertex_size_) && (i < vertex_capacity_)) {continue;}
         if(i == vertex_size_) {out << "| ";}
+        if((i >= vertex_size_) && (i < vertex_capacity_)) {continue;}
         out << std::setw(4) << i << " ";
     }
     out << std::endl;
 
     for(std::size_t i = 0, maxi = graph_.size(); i < maxi; ++i) {
-        if((i >= vertex_size_) && (i < vertex_capacity_)) {continue;}
         if(i == vertex_size_) {out << "| ";}
+        if((i >= vertex_size_) && (i < vertex_capacity_)) {continue;}
         out << std::setw(4) << graph_[i].incident_vertex << " ";
     }
     out << std::endl;
 
     for(std::size_t i = 0, maxi = graph_.size(); i < maxi; ++i) {
-        if((i >= vertex_size_) && (i < vertex_capacity_)) {continue;}
         if(i == vertex_size_) {out << "| ";}
+        if((i >= vertex_size_) && (i < vertex_capacity_)) {continue;}
         out << std::setw(4) << graph_[i].next << " ";
     }
     out << std::endl;
 
     for(std::size_t i = 0, maxi = graph_.size(); i < maxi; ++i) {
-        if((i >= vertex_size_) && (i < vertex_capacity_)) {continue;}
         if(i == vertex_size_) {out << "| ";}
+        if((i >= vertex_size_) && (i < vertex_capacity_)) {continue;}
         out << std::setw(4) << graph_[i].prev << " ";
     }
     out << std::endl;
--- combinatorics_1/main.cpp
+++ combinatorics_1/main.cpp
@@ -1,20 +1,36 @@
 #include <iostream>
 #include <iomanip>
+#include <fstream>
 
-#include "parser/driver.hpp"
 #include "kgraph.hpp"
 
+struct edge_t {
+    std::size_t v1, v2, w;
+};
+
+std::vector<edge_t> read_input(std::istream& in) {
+    std::vector<edge_t> edges;
+    while(in) {
+        edge_t tmp;
+        in >> tmp.v1;
+        if(!in) { break; } 
+        in.ignore(3);
+        in >> tmp.v2; 
+        in.ignore(1);
+        in >> tmp.w;
+        edges.push_back(tmp);
+    }
+
+    return edges;
+}
 
 int main(int argc, char** argv) {
-    yy::driver_t driver(argv[1]);
-    driver.parse();
-    std::vector<yy::edge_t> edges = driver.get_edges();
+    std::ifstream input(argv[1]);
+    std::vector<edge_t> edges = read_input(input);
 
     kgraph::kgraph_t<std::size_t, std::size_t> graph;
 
     for(auto&& edge : edges) {
-        graph.push_vertex(edge.v1);
-        graph.push_vertex(edge.v2);
         graph.push_edge(edge.v1, edge.v2, edge.w);
     }
 
