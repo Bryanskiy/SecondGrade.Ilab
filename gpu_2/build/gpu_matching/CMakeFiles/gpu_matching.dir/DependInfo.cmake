56982f905598ac6c611ac45c72cd0a0cec0a324e
Bryanskiy <ivakin.ki@physteh.edu>
2021-03-26 20:43:04 +0300

    small optimization

---
+++ gpu_2/build/gpu_matching/CMakeFiles/gpu_matching.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ivakin/prog/Ilab2_cpp/gpu_2/gpu_matching/cl_core.cpp" "/home/ivakin/prog/Ilab2_cpp/gpu_2/build/gpu_matching/CMakeFiles/gpu_matching.dir/cl_core.cpp.o"
+  "/home/ivakin/prog/Ilab2_cpp/gpu_2/gpu_matching/gpu_kmp.cpp" "/home/ivakin/prog/Ilab2_cpp/gpu_2/build/gpu_matching/CMakeFiles/gpu_matching.dir/gpu_kmp.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ gpu_2/build/gpu_matching/CMakeFiles/gpu_matching.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gpu_matching.dir/cl_core.cpp.o"
+  "CMakeFiles/gpu_matching.dir/gpu_kmp.cpp.o"
+  "libgpu_matching.a"
+  "libgpu_matching.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gpu_matching.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
